ee29d1f31f79a18a681d8d1d99f90d4e2b3a0d24
shahedruhi <shahedruhi@gmail.com>
2014-08-24 13:16:12 +0600

    trying bootstarp more

--- app/views/user/index.blade.php
+++ app/views/user/index.blade.php
@@ -13,7 +13,7 @@
 
             <thead>
             <tr>
-                <th>Name</th>
+                
                 <th>Username</th>
                 <th>Email</th>
                 <th>Date/Time Added</th>
